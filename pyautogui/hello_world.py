e4d0f0ec4b45956930dbdf612f672a77258c0b0c
DesistDaydream <39847025+DesistDaydream@users.noreply.github.com>
2022-02-13 18:30:27 +0800

    2022.2.13

--- pyautogui/hello_world.py
+++ pyautogui/hello_world.py
@@ -1,10 +1,5 @@
 import pyautogui
-import time
-import xlrd
-import pyperclip
 
-location = pyautogui.locateCenterOnScreen(
-    "pyautogui/mercenaries.png", confidence=0.9)
+location = pyautogui.locateCenterOnScreen("pyautogui/mercenaries.png", confidence=0.9)
 
-pyautogui.click(location.x, location.y, clicks=1,
-                interval=0.2, duration=0.2, button="left")
+pyautogui.click(location.x, location.y, clicks=1,interval=0.2, duration=0.2, button="left")
---
+++ pyautogui/keyborad.py
@@ -0,0 +1,7 @@
+import pyautogui
+
+# 输入 Hello World! 字符串
+pyautogui.typewrite('Hello World!')
+
+# 按下 Win 键
+pyautogui.press('win')
