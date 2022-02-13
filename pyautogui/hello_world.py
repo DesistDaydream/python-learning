97fa892a23a6a1b23a89132dda25def73c814306
DesistDaydream <39847025+DesistDaydream@users.noreply.github.com>
2022-02-13 19:08:05 +0800

    pyautogui 库练习移到单独的仓库中

--- pyautogui/hello_world.py
+++
@@ -1,5 +0,0 @@
-import pyautogui
-
-location = pyautogui.locateCenterOnScreen("pyautogui/mercenaries.png", confidence=0.9)
-
-pyautogui.click(location.x, location.y, clicks=1,interval=0.2, duration=0.2, button="left")
--- pyautogui/keyborad.py
+++
@@ -1,7 +0,0 @@
-import pyautogui
-
-# 输入 Hello World! 字符串
-pyautogui.typewrite('Hello World!')
-
-# 按下 Win 键
-pyautogui.press('win')
