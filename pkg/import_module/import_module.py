cde2492e0d73b8f16ad3e869ded05dba3759d45b
DesistDaydream <373406000@qq.com>
2022-03-31 16:37:20 +0800

    添加 module and package

---
+++ pkg/import_module/import_module.py
@@ -0,0 +1,8 @@
+import sys
+
+sys.path.append("/mnt/d/Projects/DesistDaydream/python-learning/pkg")
+
+import module_and_package.hello as hello
+
+print(sys.path)
+print(hello.test())
---
+++ pkg/import_module/import_package.py
@@ -0,0 +1,6 @@
+import sys
+
+from ..module_and_package import hello
+
+print(sys.path)
+print(hello.test())
