734732c32b54da3e61b020c25fee1b468bef2704
DesistDaydream <373406000@qq.com>
2022-04-01 14:38:39 +0800

    Update class.py

--- pkg/7_class_and_instance/class.py
+++ pkg/7_class_and_instance/class.py
@@ -8,17 +8,31 @@ class Student(object):
         self.name = name
         self.score = ""
 
+    # 方法，获取实例化后的 name 属性的值。即获取学生的名字
+    def getName(self):
+        return self.name
+
     # 方法，根据 数学 与 计算机 的成绩计算总成绩
     def sum(self, math, computer):
         self.score = math + computer
 
     # 方法，输出某个学生的总成绩
-    def print_score(self):
+    def printScore(self):
         print("%s 的分数为: %s" % (self.name, self.score))
+
+    # 方法，根据成绩得出等级
+    def getGrade(self):
+        if self.score >= 90:
+            return "A"
+        elif self.score >= 60:
+            return "B"
+        else:
+            return "C"
 
 
 # 实例化
 bart = Student("Bart Simpson")
 
 bart.sum(100, 99)
-bart.print_score()
+bart.printScore()
+print("%s 的评级: %s" % (bart.getName(), bart.getGrade()))
