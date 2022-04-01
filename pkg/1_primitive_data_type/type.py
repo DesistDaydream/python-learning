97ba64c2080d3ecdd2d046f658ea074535c903dc
DesistDaydream <373406000@qq.com>
2022-04-01 15:05:54 +0800

    继承

--- pkg/1_primitive_data_type/type.py
+++ pkg/1_primitive_data_type/type.py
@@ -1,11 +1,21 @@
-# 格式化输出
-# 使用 % 符号
-formatString = "World"
-formatInt = 3
-formatFloat = 3.1415926
-print("Hello%s%s" % (",", formatString))
-print("%.2f" % (formatFloat))
-# 使用 format 函数
-print("Hello{}{}".format(",", formatString))
-print("Hello{1}{1}".format(",", formatString))
-print("{:.3f}".format(formatFloat))
+stringType = "Hello World!"
+
+# 判断类型
+print("字符串类型: ", type(stringType))
+
+
+def formatOutput():
+    # 格式化输出
+    # 使用 % 符号
+    formatString = "World"
+    formatInt = 3
+    formatFloat = 3.1415926
+    print("Hello%s%s" % (",", formatString))
+    print("%.2f" % (formatFloat))
+    # 使用 format 函数
+    print("Hello{}{}".format(",", formatString))
+    print("Hello{1}{1}".format(",", formatString))
+    print("{:.3f}".format(formatFloat))
+
+
+# formatOutput()
--- pkg/7_class_and_instance/inheritance.py
+++ pkg/7_class_and_instance/inheritance.py
@@ -2,10 +2,23 @@
 # -*- coding: utf-8 -*-
 
 # object 表示该类从哪个类继承下来。如果没有合适的继承，就是 object，这是所有类最终都会继承的类。
-class Student(object):
+class Animal(object):
+    def run(self):
+        print("Animal is running...")
+
+
+# Dog 类继承了 Animal
+class Dog(Animal):
     pass
 
 
-bart = Student()
+# Cat 类继承了 Animal
+class Cat(Animal):
+    pass
 
-print(bart)
+
+dog = Dog()
+dog.run()
+
+cat = Cat()
+cat.run()
