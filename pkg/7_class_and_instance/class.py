58b0979ec18b8ca7fca1bea873fa71123e6c972e
DesistDaydream <373406000@qq.com>
2022-04-01 14:30:54 +0800

    添加类

--- pkg/7_class_and_instance/class.py
+++ pkg/7_class_and_instance/class.py
@@ -1,2 +1,24 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+# 使用 class 关键字定义一个名为 Student 的类
+class Student(object):
+    # 初始化属性
+    def __init__(self, name):
+        self.name = name
+        self.score = ""
+
+    # 方法，根据 数学 与 计算机 的成绩计算总成绩
+    def sum(self, math, computer):
+        self.score = math + computer
+
+    # 方法，输出某个学生的总成绩
+    def print_score(self):
+        print("%s 的分数为: %s" % (self.name, self.score))
+
+
+# 实例化
+bart = Student("Bart Simpson")
+
+bart.sum(100, 99)
+bart.print_score()
---
+++ pkg/7_class_and_instance/inheritance.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# object 表示该类从哪个类继承下来。如果没有合适的继承，就是 object，这是所有类最终都会继承的类。
+class Student(object):
+    pass
+
+
+bart = Student()
+
+print(bart)
