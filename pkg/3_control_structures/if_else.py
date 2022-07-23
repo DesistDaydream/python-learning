04a637a64db1a9c4b41b489b4bca8996d091a231
DesistDaydream <39847025+DesistDaydream@users.noreply.github.com>
2022-07-24 00:17:15 +0800

    Update if_else.py

---
+++ pkg/3_control_structures/if_else.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+flag = False
+name = "DesistDaydream"
+if name == "python":  # 判断变量是否为 python
+    flag = True  # 条件成立时设置标志为真
+    print("Hello World")  # 并输出欢迎信息
+else:
+    print(name)  # 条件不成立时输出变量名称
