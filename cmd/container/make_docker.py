f880065aeea4433395ac79cc206c3b77aa78201d
LiChenhao <39847025+DesistDaydream@users.noreply.github.com>
2023-02-24 17:25:31 +0800

    Update make_docker.py

--- cmd/container/make_docker.py
+++ cmd/container/make_docker.py
@@ -8,6 +8,8 @@ from urllib import request
 import os, sys, stat
 import tarfile
 import logging
+import json
+
 
 parser = argparse.ArgumentParser(description="Containerd 安装包生成工具")
 parser.add_argument("-d", "--download-dir", default="/tmp/downloads", help="下载目录")
@@ -84,19 +86,29 @@ def extracting(tar_path: str, target_path: str):
 class files_handler:
     def __init__(self):
         # 文件名
+        self.ContainerdServiceFileName = "containerd.service"
         self.DockerFileName = "docker-{}.tgz".format(flags.DockerVersion)
         self.DockerServiceFileName = "docker.service"
+        self.DockerSocketFileName = "docker.socket"
         self.DockerCompletionFileName = "docker"
         self.DockerComposeFileName = "docker-compose"
         # 文件保存路径
+        self.ContainerdServiceFilePath = "{}/{}".format(flags.DownloadDir, self.ContainerdServiceFileName)
         self.DockerFilePath = "{}/{}".format(flags.DownloadDir, self.DockerFileName)
         self.DockerServiceFilePath = "{}/{}".format(flags.DownloadDir, self.DockerServiceFileName)
+        self.DockerSocketFilePath = "{}/{}".format(flags.DownloadDir, self.DockerSocketFileName)
         self.DockerCompletionFilePath = "{}/{}".format(flags.DownloadDir, self.DockerCompletionFileName)
         self.DockerComposeFilePath = "{}/{}".format(flags.DownloadDir, self.DockerComposeFileName)
         # 下载文件的 URL
+        self.ContainerdServiceURL = "https://raw.githubusercontent.com/containerd/containerd/main/{}".format(
+            self.ContainerdServiceFileName
+        )
         self.DockerURL = "https://download.docker.com/linux/static/stable/x86_64/{}".format(self.DockerFileName)
         self.DockerServiceURL = "https://raw.githubusercontent.com/moby/moby/v{}/contrib/init/systemd/{}".format(
             flags.DockerVersion, self.DockerServiceFileName
+        )
+        self.DockerSocketURL = "https://raw.githubusercontent.com/moby/moby/v{}/contrib/init/systemd/{}".format(
+            flags.DockerVersion, self.DockerSocketFileName
         )
         self.DockerCompletionURL = (
             "https://raw.githubusercontent.com/docker/cli/v{}/contrib/completion/bash/docker".format(
@@ -110,7 +122,9 @@ class files_handler:
         )
 
     def downloadFiles(self):
+        logging.debug("Containerd Service 下载 URL: {}".format(self.ContainerdServiceURL))
         logging.debug("Docker Service 下载 URL: {}".format(self.DockerServiceURL))
+        logging.debug("Docker Socket 下载 URL: {}".format(self.DockerSocketURL))
         logging.debug("Docker Completion 文件 下载 URL: {}".format(self.DockerCompletionURL))
         logging.debug("Docker 下载 URL: {}".format(self.DockerURL))
         logging.debug("Docker Compose 下载 URL: {}".format(self.DockerComposeURL))
@@ -121,8 +135,12 @@ class files_handler:
 
         # 下载 tar 包
         # TODO: 验证下载结果
+        if not os.path.exists(self.ContainerdServiceFilePath):
+            request.urlretrieve(self.ContainerdServiceURL, self.ContainerdServiceFilePath)
         if not os.path.exists(self.DockerServiceFilePath):
             request.urlretrieve(self.DockerServiceURL, self.DockerServiceFilePath)
+        if not os.path.exists(self.DockerSocketFilePath):
+            request.urlretrieve(self.DockerSocketURL, self.DockerSocketFilePath)
         if not os.path.exists(self.DockerCompletionFilePath):
             request.urlretrieve(self.DockerCompletionURL, self.DockerCompletionFilePath)
         if not os.path.exists(self.DockerFilePath):
@@ -136,13 +154,24 @@ class files_handler:
         os.makedirs(flags.WorkDir + "/etc/systemd/system", exist_ok=True)
         os.makedirs(flags.WorkDir + "/usr/share/bash-completion/completions", exist_ok=True)
         os.makedirs(flags.WorkDir + "/usr/local/bin", exist_ok=True)
+        os.makedirs(flags.WorkDir + "/usr/bin", exist_ok=True)
+        os.makedirs(flags.WorkDir + "/etc/docker", exist_ok=True)
+
+        # 拷贝 Containerd 的 Service 文件
+        if not os.path.exists(flags.WorkDir + "/etc/systemd/system/" + self.ContainerdServiceFileName):
+            shutil.move(self.ContainerdServiceFilePath, flags.WorkDir + "/etc/systemd/system/")
 
         # 提取 Docker
-        extracting(self.DockerFilePath, flags.WorkDir + "/usr/bin/")
+        extracting(self.DockerFilePath, flags.WorkDir)
+        docker_files = os.listdir(flags.WorkDir + "/docker/")
+        for file in docker_files:
+            shutil.move(flags.WorkDir + "/docker/" + file, flags.WorkDir + "/usr/bin/" + file)
 
         # 将 Docker 的 Service 文件拷贝到指定目录
         if not os.path.exists(flags.WorkDir + "/etc/systemd/system/" + self.DockerServiceFileName):
             shutil.copy2(self.DockerServiceFilePath, flags.WorkDir + "/etc/systemd/system/")
+        if not os.path.exists(flags.WorkDir + "/etc/systemd/system/" + self.DockerSocketFileName):
+            shutil.copy2(self.DockerSocketFilePath, flags.WorkDir + "/etc/systemd/system/")
 
         # 将 Docker 的命令行补全文件拷贝到指定目录
         if not os.path.exists(
@@ -158,25 +187,26 @@ class files_handler:
                 stat.S_IRWXU + stat.S_IRGRP + stat.S_IXGRP + stat.S_IROTH,
             )
 
-        # # 提取 CNI Plugins
-        # extracting(self.CNIpluginFilePath, flags.WorkDir + "/opt/cni/bin/")
-
-        # # 提取 Nerdctl
-        # extracting(self.NerdctlFilePath, flags.WorkDir + "/usr/local/bin/")
-
 
 def handleFiles(flags: cli_flags):
-    # 生成 nerdctl 的命令行补全
-    if not os.path.exists(flags.WorkDir + "/usr/share/bash-completion/completions"):
-        os.makedirs(flags.WorkDir + "/usr/share/bash-completion/completions")
-    nedctlCMD = "{0}/usr/local/bin/nerdctl completion bash > {0}/usr/share/bash-completion/completions/nerdctl".format(
-        flags.WorkDir
-    )
-    # 非 root 环境执行这个命令会有一些 WARN，忽略即可
-    os.system(nedctlCMD)
+    config = {
+        "experimental": True,
+        "registry-mirrors": ["https://ac1rmo5p.mirror.aliyuncs.com"],
+        "exec-opts": ["native.cgroupdriver=systemd"],
+        "default-address-pools": [{"base": "10.38.0.0/16", "size": 24}],
+        "live-restore": True,
+        "log-driver": "json-file",
+        "log-opts": {"max-size": "5m", "max-file": "5"},
+        "storage-driver": "overlay2",
+        "storage-opts": ["overlay2.override_kernel_check=true"],
+    }
 
-    # 生成 crictl 配置文件
-    # TODO: 有必要做嘛？
+    json_str = json.dumps(config, indent=2)
+    with open(flags.WorkDir + "/etc/docker/daemon.json", "w") as f:
+        f.write(json_str)
+
+
+# 定义要写入的配置字典
 
 
 if __name__ == "__main__":
@@ -191,6 +221,6 @@ if __name__ == "__main__":
     # 提取文件
     files.extractingFiles()
     # 提取文件后，处理归档目录以满足归档条件
-    # handleFiles(flags)
+    handleFiles(flags)
     # 归档，生成归档文件
     archiving(flags.WorkDir, flags.DownloadDir + "/docker-ehualu-{}.tar.gz".format(flags.DockerVersion))
