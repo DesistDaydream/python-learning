6e9b6d60af95c6a84ccc8783ad3af287d8398cb9
LiChenhao <39847025+DesistDaydream@users.noreply.github.com>
2023-04-09 01:10:14 +0800

    Update for.py

--- pkg/3_control_structures/for.py
+++ pkg/3_control_structures/for.py
@@ -4,12 +4,13 @@ from collections.abc import Iterable
 
 listType = ["List", "DataType", 1, 3.1415926]
 
-for l in listType:
-    print(l)
+for value in listType:
+    print(value)
 
-for i, v in enumerate(listType):
-    print("元素号: ", i)
-    print("值: ", v)
+# 通过 enumerate() 函数可以获取元素的索引
+for index, value in enumerate(listType):
+    print("元素号: ", index)
+    print("值: ", value)
 
 # 判断一个对象是否是可迭代的
 print(isinstance(listType, Iterable))
