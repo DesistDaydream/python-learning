b2ebe17ea4ae02e08935f984e3658751015da395
DesistDaydream <39847025+DesistDaydream@users.noreply.github.com>
2024-03-26 14:17:43 +0800

    module and package

--- pkg/module_and_package/module_and_package_of_use.py
+++ pkg/module_and_package/module_and_package_of_use.py
@@ -12,3 +12,9 @@ from package_two import import_package as indirect_import
 # import_package.notUseSysPath()
 print("通过 import 关键字直接导入包中的模块:", direct_import.PrintSomething())
 print("通过 from 关键字间接导入包中的模块:", indirect_import.PrintSomething())
+
+# 若仅导入目录，则需要在 package_two/__init__.py 中添加对函数的引用代码: `from .import_package import PrintSomething`
+# 否则将会提示: AttributeError: module 'package_two' has no attribute 'PrintSomething'
+import package_two
+print(package_two.PrintSomething())
+print(package_two.useSysPath())
---
+++ pkg/module_and_package/package_two/__init__.py
@@ -0,0 +1,3 @@
+# 当导入包时只指定到目录的路径而不是具体的文件，则需要在目录下的 __init__.py 文件中添加如下内容，以导出想要的函数
+# from .import_package import PrintSomething
+# from .import_package import useSysPath
